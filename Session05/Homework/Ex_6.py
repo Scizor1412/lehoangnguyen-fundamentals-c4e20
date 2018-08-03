cb9aadcd56ac5fde133af46c955e452d2dab6d6d
Scizor1412 <nguyenlehoang212@gmail.com>
2018-08-04 03:33:51 +0700

    second commit

--- Session05/Homework/Ex_6.py
+++ Session05/Homework/Ex_6.py
@@ -24,52 +24,82 @@ obstacles = [
 
 player = {"x" : 1, "y" : 1}
 
-for y in range (map_size["size_y"]):
-    for x in range (map_size["size_x"]):
+loop = True
 
-        player_is_here = False
-        if y == player["y"] and x == player["x"]:
-            player_is_here = True
-        
-        box_is_here = False
-        for box in boxes:
-            if y == box["y"] and x == box["x"]:
-                box_is_here = True
+while loop:
+    for y in range (map_size["size_y"]):
+        for x in range (map_size["size_x"]):
 
-        obstacle_is_here = False
-        for obstacle in obstacles:
-            if y == obstacle["y"] and x == obstacle["x"]:
-                obstacle_is_here = True
+            player_is_here = False
+            if y == player["y"] and x == player["x"]:
+                player_is_here = True
+            
+            box_is_here = False
+            for box in boxes:
+                if y == box["y"] and x == box["x"]:
+                    box_is_here = True
 
-        destination_is_here = False
-        for destination in destinations:
-            if y == destination["y"] and x == destination["x"]:
-                destination_is_here = True
-        
-        if player_is_here:
-            print ("P ", end=" ")
-        elif box_is_here:
-            print ("B ", end=" ")
-        elif obstacle_is_here:
-            print ("O ", end=" ")
-        elif destination_is_here:
-            print ("D ", end=" ")
+            obstacle_is_here = False
+            for obstacle in obstacles:
+                if y == obstacle["y"] and x == obstacle["x"]:
+                    obstacle_is_here = True
+
+            destination_is_here = False
+            for destination in destinations:
+                if y == destination["y"] and x == destination["x"]:
+                    destination_is_here = True
+            
+            if player_is_here:
+                print ("P ", end=" ")
+            elif box_is_here:
+                print ("B ", end=" ")
+            elif obstacle_is_here:
+                print ("O ", end=" ")
+            elif destination_is_here:
+                print ("D ", end=" ")
+            else:
+                print ("- ", end=" ")
+        print ("")
+
+    dx = 0
+    dy = 0
+
+    move = input ("Your move? ")
+    if move.lower() == "w":
+        dy = -1
+    elif move.lower() == "d":
+        dx = 1
+    elif move.lower() == "a":
+        dx = -1
+    elif move.lower() == "s":
+        dy = 1
+    else:
+        print ("Shit!!!")
+
+    move_player = False
+    move_box = False
+    for box in boxes:
+        for obs in obstacles:
+            if 0 <= player["x"] + dx <= map_size["size_x"]-1 and 0 <= player["y"] + dy <= map_size["size_y"]-1:
+                
+                if player["x"] + dx == box["x"] and player["y"] + dy == box["y"]:
+                    if 0 <= box["x"] + dx <= map_size["size_x"]-1 and 0 <= box["y"] + dy <= map_size["size_y"]-1:
+                        if box["x"] + dx != obs["x"] and box["y"] + dy != obs["y"]:
+                            if box["x"] + dx != box["x"] and box["y"] + dy != box["y"]:
+                                move_player=True
+                                move_box=True
+                elif player["x"] + dx != obs["x"] and player["y"] + dy != obs["y"]:
+                    move_player=True
+                    
+    if move_player:
+        player["y"] += dy
+        player["x"] += dx
+    for box in boxes:
+        if move_box:
+            box["y"] += dy
+            box["x"] += dx
+        if box not in destinations:
+            continue
         else:
-            print ("- ", end=" ")
-    print ("")
-
-dx = 0
-dy = 0
-
-move = input ("Your move? ")
-if move.lower() == "w":
-    dy = -1
-elif move.lower() == "d":
-    dx = 1
-elif move.lower() == "a":
-    dx = -1
-elif move.lower() == "s":
-    dy = 1
-if 0 <= (player["x"] + dx) <= (map_size["size_x"]) and 0 <= (player["y"] + dx) <= (map_size["size_y"]):
-    player["y"] += dy
-    player["x"] += dx
+            print ("You win!!!")
+            break
---
+++ Session05/Homework/Ex_6_2.py
@@ -0,0 +1,126 @@
+map_size = {
+    "size_x" : 6,
+    "size_y" : 6
+}
+
+boxes = [
+    {"x" : 0, "y" : 2},
+    {"x" : 1, "y" : 2},
+    {"x" : 2, "y" : 2},
+    {"x" : 3, "y" : 2}
+]
+
+destinations = [
+    {"x" : 0, "y" : 3},
+    {"x" : 1, "y" : 4},
+    {"x" : 0, "y" : 5},
+    {"x" : 3, "y" : 5}
+]
+
+obstacles = [
+    {"x" : 1, "y" : 3},
+    {"x" : 4, "y" : 5}
+]
+
+player = {"x" : 1, "y" : 1}
+
+loop = True
+
+while loop:
+
+#Ve hinh
+    for y in range (map_size["size_y"]):
+        for x in range (map_size["size_x"]):
+
+            player_is_here = False
+            if y == player["y"] and x == player["x"]:
+                player_is_here = True
+            
+            box_is_here = False
+            for box in boxes:
+                if y == box["y"] and x == box["x"]:
+                    box_is_here = True
+
+            obstacle_is_here = False
+            for obstacle in obstacles:
+                if y == obstacle["y"] and x == obstacle["x"]:
+                    obstacle_is_here = True
+
+            destination_is_here = False
+            for destination in destinations:
+                if y == destination["y"] and x == destination["x"]:
+                    destination_is_here = True
+            
+            if player_is_here:
+                print ("P ", end=" ")
+            elif box_is_here:
+                print ("B ", end=" ")
+            elif obstacle_is_here:
+                print ("O ", end=" ")
+            elif destination_is_here:
+                print ("D ", end=" ")
+            else:
+                print ("- ", end=" ")
+        print ("")
+#Het ve hinh
+#win
+    win = True
+    for box in boxes:
+        if box not in destinations:
+            win = False
+    if win:
+        print ("You win!!!")
+        break
+#Het win
+#move
+    move = input ("Your move? ")
+    dx = 0
+    dy = 0
+
+    if move.lower() == "w":
+        dy = -1
+        print ("up")
+    elif move.lower() == "d":
+        dx = 1
+        print ("right")
+    elif move.lower() == "a":
+        dx = -1
+        print ("left")
+    elif move.lower() == "s":
+        dy = 1
+        print ("down")
+    else:
+        print ("Shit!!!")
+#het move
+#lap
+    if 0 <= player["x"] + dx < map_size["size_x"] and 0 <= player["y"] + dy < map_size["size_y"]:
+        player["x"] += dx
+        player["y"] += dy
+        for box in boxes:
+            for obs in obstacles:
+                if player["x"] == obs["x"] and player["y"] == obs["y"]:
+                    print ("Invalid move!!!")
+                    player["x"] -= dx
+                    player["y"] -= dy
+                elif player["x"] == box["x"] and player["y"] == box["y"]:
+                    if 0<= box["x"] + dx < map_size["size_x"] and 0 <= box["y"] + dy < map_size["size_y"]:
+                        box["x"] += dx
+                        box["y"] += dy
+                        duplicate = False
+                        for i in range (len(boxes)):
+                            for j in range (len(boxes)):
+                                if boxes[i] == boxes[j] and i != j:
+                                    duplicate = True
+                        if duplicate:
+                            print ("Invalid move!!!")
+                            player["x"] -= dx
+                            player["y"] -= dy
+                            box["x"] -= dx
+                            box["y"] -= dy
+                        elif box["x"] == obs["x"] and box["y"]== obs["y"]:
+                            print ("Invalid move!!!")
+                            player["x"] -= dx
+                            player["y"] -= dy
+                            box["x"] -= dx
+                            box["y"] -= dy
+                        break
