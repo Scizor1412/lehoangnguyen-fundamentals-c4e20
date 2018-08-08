f434ce65d67cf0c5b088761a69c0b020a0eae459
Scizor1412 <nguyenlehoang212@gmail.com>
2018-08-09 00:07:53 +0700

    initial commit

--- Lab1/Homework/customers_db.py
+++ Lab1/Homework/customers_db.py
@@ -18,7 +18,7 @@ word_of_mouth = customers.find({"ref": "wom"}).count()
 print (ads)
 print (events)
 print (word_of_mouth)
-
+# robo 3t
 labels = ["ads", "events", "word_of_mouth"]
 values = [1938, 3900, 1131]
 colors = ["red", "blue", "black"]
